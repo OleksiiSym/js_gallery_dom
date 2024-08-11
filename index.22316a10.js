50ba64126dbfcdf40d727c6e142045e9f3955d2e
Oleksii Symonets <alex.simonets@gmail.com>
2024-08-11 16:36:45 +0300

    make build

---
+++ index.22316a10.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.querySelector("#thumbs").addEventListener("click",function(t){t.preventDefault();var r=t.target.closest("a");e.setAttribute("src",r.href)});
+//# sourceMappingURL=index.22316a10.js.map
